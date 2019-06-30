983f222c5ef935a53147f590d295cea8f749197e
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-30 20:25:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,10 +31,9 @@ class Song
     genre_hash = {}
     @@genres.each do |genre|
       if genre_hash.key?(genre)
-        binding.pry
-      genre_hash = genre_hash[genre] +=1
+      genre_hash[genre] +=1
       else
-      genre_hash = genre_hash[genre] = 1
+      genre_hash[genre] = 1
       end
     end
     #genre_hash
